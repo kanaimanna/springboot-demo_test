c9d2b0d893bc117f7c968d85cb93c2410059f9dd
kanaimanna <kanaimanna@gmail.com>
2024-02-18 20:38:37 +0530

    first commit

---
+++ src/main/java/com/star/agile/assignment/springbootdemo_test/SpringbootDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemo_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/agile/assignment/springbootdemo_test/SpringbootDemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemo_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
